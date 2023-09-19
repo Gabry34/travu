3568521714afe908756895ab1046ef4ffd118570
Gabriele Conciauro <gabrieleconciauro34@gmail.com>
2023-09-19 16:32:09 +0200

    commit

---
+++ src/app/components/HomeImage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeImage = () => {
+  return <div>HomeImage</div>;
+};
+
+export default HomeImage;
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import HomeImage from "./components/HomeImage";
+
+export default function Home() {
+  return (
+    <div>
+      <HomeImage />
+    </div>
+  );
+}
