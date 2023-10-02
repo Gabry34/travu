1099442124312db33dfc63634ec7b110a46c93fe
Gabriele Conciauro <gabrieleconciauro34@gmail.com>
2023-10-02 23:58:24 +0200

    login and registration integration

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@
 
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Provider from "../../context/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`bg-white ${inter.className}`}>{children}</body>
+      <Provider>
+        <body className={`bg-white ${inter.className}`}>{children}</body>
+      </Provider>
     </html>
   );
 }
