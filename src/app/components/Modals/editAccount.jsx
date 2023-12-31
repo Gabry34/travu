e72c2d1d7fcc9584de6febe21d76badcad1a0356
Gabry34 <gabrieleconciauro34@gmail.com>
2023-12-31 19:33:20 +0100

    commit

--- src/app/components/Modals/editAccount.jsx
+++ src/app/components/Modals/editAccount.jsx
@@ -147,9 +147,8 @@ export default function EditAccount({ userInfoId }) {
               />
             </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <h1 className="text-white">{session?.user?.name}</h1>
-            {/* <h1 className="text-white">Change name</h1>
+          {/* <div className="flex flex-col gap-1">
+            <h1 className="text-white">Change name</h1>
             <input
               type="text"
               defaultValue={session?.user.name}
@@ -157,8 +156,8 @@ export default function EditAccount({ userInfoId }) {
                 setName(e.target.value);
               }}
               className="w-full rounded-md px-2 py-1 outline-none bg-[#3B3B3B] text-white"
-            /> */}
-          </div>
+            />
+          </div> */}
           {session?.user?.hashedPassword !==
             "no password: registered with google" && (
             <div className="flex flex-col gap-2">
